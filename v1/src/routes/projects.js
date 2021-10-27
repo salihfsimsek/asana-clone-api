082d73c686a3a1844bb123f336ca6e27b45bcb79
salihsimseek@gmail.com <salihsimseek@gmail.com>
2021-10-27 22:21:20 +0300

    validation middleware created and added validation on project create route

--- v1/src/routes/projects.js
+++ v1/src/routes/projects.js
@@ -1,12 +1,14 @@
 //Validations
+const schemas = require('../validations/projects')
 
 //Validate middleware
+const validate = require('../middlewares/validate')
 
 const router = require('express').Router()
 const { create, index } = require('../controllers/projects')
 
 router.get('/', index)
 
-router.post('/',create)
+router.post('/', validate(schemas.createValidation), create)
 
 module.exports = router
