dfaea74f5537745d17f35706db3147df5e1ca18d
salihsimseek@gmail.com <salihsimseek@gmail.com>
2021-12-14 22:45:38 +0300

    task model and task logger created

---
+++ v1/src/models/tasks.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose')
+const logger = require('./scripts/logger/tasks')
+
+const TaskSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: String,
+    assigned_to: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'User'
+    },
+    due_date: Date,
+    statuses: [String],
+    section_id: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'Section'
+    },
+    project_id: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'Project'
+    },
+    user_id: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'User'
+    },
+    order: Number,
+    isCompleted: Boolean,
+    comments: [{
+        value: String,
+        created_at: Date,
+        updated_at: Date,
+        user_id: {
+            type: mongoose.SchemaTypes.ObjectId,
+            ref: 'User'
+        },
+    }],
+    media: [String],
+    sub_tasks: [{
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'Task'
+    }]
+}, { timestamps: true, versionKey: false })
+
+TaskSchema.post('save', () => {
+    logger.log({
+        level: 'info',
+        message: doc
+    })
+})
+
+const TaskModel = mongoose.model('Task', TaskSchema)
+
+module.exports = TaskModel
--- v1/src/scripts/logger/sections.js
+++ v1/src/scripts/logger/sections.js
@@ -3,7 +3,7 @@ const winston = require('winston')
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.json(),
-    defaultMeta: { service: 'project-service' },
+    defaultMeta: { service: 'section-service' },
     transports: [
         new winston.transports.File({ filename: './v1/src/logs/sections/error.log', level: 'error' }),
         new winston.transports.File({ filename: './v1/src/logs/sections/info.log', level: 'info' }),
