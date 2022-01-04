86d4fb8eb08629bf539ddbb9ffbee83ccb76f68b
salihsimseek@gmail.com <salihsimseek@gmail.com>
2022-01-04 22:11:07 +0300

    sections error messages fixed

--- v1/src/controllers/sections.js
+++ v1/src/controllers/sections.js
@@ -1,44 +1,47 @@
 const SectionService = require('../services/sections')
 const httpStatus = require('http-status')
 
-const index = async (req, res) => {
-    if (!req.params?.projectId) return res.status(httpStatus.NOT_FOUND).send({ error: 'Proje bilgisi yok' })
+const ApiError = require('../errors/apiError')
+
+const index = async (req, res, next) => {
     try {
         const sections = await SectionService.list({ project_id: req.params.projectId })
-        console.log(sections)
         res.status(httpStatus.OK).send(sections)
     } catch (err) {
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err)
+        next(new ApiError(err?.message))
     }
 }
 
-const create = async (req, res) => {
+const create = async (req, res, next) => {
     req.body.user_id = req.user._id
     try {
         const createdSection = await SectionService.create(req.body)
         res.status(httpStatus.CREATED).send(createdSection)
     } catch (err) {
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err)
+        next(new ApiError(err?.message))
     }
 }
 
-const update = async (req, res) => {
-    if (!req.params.id) return res.status(httpStatus.NOT_FOUND).send({ message: 'Not Found' })
+const update = async (req, res, next) => {
     try {
         const updatedSection = await SectionService.update({ _id: req.params.id }, req.body)
+        if (!updatedSection)
+            return next(new ApiError('Section not found', 404))
         res.status(200).send(updatedSection)
     } catch (err) {
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err)
+        next(new ApiError(err?.message))
     }
 }
 
-const deleteSection = async (req, res) => {
+const deleteSection = async (req, res, next) => {
     if (!req.params.id) return res.status(httpStatus.NOT_FOUND).send({ message: 'Not Found' })
     try {
-        await SectionService.delete({ _id: req.params.id })
+        const deletedSection = await SectionService.delete({ _id: req.params.id })
+        if (!deletedSection)
+            return next(new ApiError('Section not found', 404))
         res.status(httpStatus.OK).send({ message: 'Succesfully Deleted' })
     } catch (err) {
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err)
+        next(new ApiError(err?.message))
     }
 }
 
