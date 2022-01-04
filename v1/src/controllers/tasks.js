e122f76d4128f21b2d7c4b1e5ce64a0df95abf00
salihsimseek@gmail.com <salihsimseek@gmail.com>
2022-01-04 22:16:27 +0300

    tasks error messages fixed

--- v1/src/controllers/tasks.js
+++ v1/src/controllers/tasks.js
@@ -1,94 +1,98 @@
 const TaskService = require('../services/tasks')
 const httpStatus = require('http-status')
 
-const index = async (req, res) => {
-    if (!req.params.id) return res.status(httpStatus.BAD_REQUEST).send({ message: 'ID is required.' })
+const ApiError = require('../errors/apiError')
 
+const index = async (req, res, next) => {
     try {
         const tasks = await TaskService.list({ _id: req.params.id })
         res.status(httpStatus.OK).send(tasks)
     } catch (err) {
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ message: err.message })
+        next(new ApiError(err?.message))
     }
 }
 
-const create = async (req, res) => {
+const create = async (req, res, next) => {
     req.body.user_id = req.user._id
     try {
         const createdTask = await TaskService.create(req.body)
         res.status(httpStatus.CREATED).send(createdTask)
     } catch (err) {
-        console.log(err)
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err)
+        next(new ApiError(err?.message))
     }
 }
 
-const update = async (req, res) => {
-    if (!req.params.id) return res.status(httpStatus.NOT_FOUND).send({ message: 'Not Found' })
+const update = async (req, res, next) => {
     try {
         const updatedTask = await TaskService.update({ _id: req.params.id }, req.body)
+        if (!updatedTask)
+            return next(new ApiError('Task not found', 404))
         res.status(200).send(updatedTask)
     } catch (err) {
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err)
+        next(new ApiError(err?.message))
     }
 }
 
-const deleteTask = async (req, res) => {
-    if (!req.params.id) return res.status(httpStatus.NOT_FOUND).send({ message: 'Not Found' })
+const deleteTask = async (req, res, next) => {
     try {
-        await TaskService.remove({ _id: req.params.id })
+        const deletedTask = await TaskService.remove({ _id: req.params.id })
+        if (!deletedTask)
+            return next(new ApiError('Task not found', 404))
         res.status(httpStatus.OK).send({ message: 'Succesfully Deleted' })
     } catch (err) {
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err)
+        next(new ApiError(err?.message))
     }
 }
 
-const makeComment = async (req, res) => {
+const makeComment = async (req, res, next) => {
     req.body.user_id = req.user
     try {
         const currentTask = await TaskService.findOne({ _id: req.params.id })
+        if (!currentTask)
+            return next(new ApiError('Task not found', 404))
         currentTask.comments.push(req.body)
         const updatedTask = await currentTask.save()
         res.status(200).send(updatedTask)
     } catch (err) {
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err)
+        next(new ApiError(err?.message))
     }
 }
 
-const deleteComment = async (req, res) => {
+const deleteComment = async (req, res, next) => {
     try {
         const updatedTask = await TaskService.update(req.params.id, { $pull: { comments: { _id: req.params.commentId } } })
+        if (!updatedTask)
+            return next(new ApiError('Task not found', 404))
         res.status(200).send(updatedTask)
     } catch (err) {
-        console.log(err)
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err)
+        next(new ApiError(err?.message))
     }
 }
 
-const addSubTask = async (req, res) => {
-    if (!req.params.id) return res.status(httpStatus.BAD_REQUEST).send({ message: 'ID not found' })
+const addSubTask = async (req, res, next) => {
     req.body.user_id = req.user._id
     try {
         const mainTask = await TaskService.findOne({ _id: req.params.id })
+        if (!mainTask)
+            return next(new ApiError('Task not found'))
         const newSubTask = await TaskService.create(req.body)
         mainTask.sub_tasks.push(newSubTask)
         await mainTask.save()
         res.status(httpStatus.CREATED).send(mainTask)
 
     } catch (err) {
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ message: err.message })
+        next(new ApiError(err?.message))
     }
 }
 
-const fetchTask = async (req, res) => {
-    if (!req.params.id) return res.status(httpStatus.BAD_REQUEST).send({ message: 'ID not found' })
-
+const fetchTask = async (req, res, next) => {
     try {
         const task = await TaskService.findOne({ _id: req.params.id }, true)
-        if (!task) return res.status(httpStatus.NOT_FOUND).send({ message: 'Task not found' })
+        if (!task)
+            return next(new ApiError('Task not found', 404))
         res.status(httpStatus.OK).send(task)
     } catch (err) {
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ message: err.message })
+        next(new ApiError(err?.message))
     }
 }
 
