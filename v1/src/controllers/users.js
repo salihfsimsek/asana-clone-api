502fe096dcec4315969a1c49efeca1d57aa4ba4e
salihsimseek@gmail.com <salihsimseek@gmail.com>
2022-01-04 22:23:06 +0300

    user error messages fixed

--- v1/src/controllers/users.js
+++ v1/src/controllers/users.js
@@ -7,9 +7,11 @@ const path = require('path')
 const UserService = require('../services/users')
 const ProjectService = require('../services/projects')
 
+const ApiError = require('../errors/apiError')
+
 const { passwordToHash, generateAccessToken, generateRefreshToken } = require('../scripts/utils/helper')
 
-const create = async (req, res) => {
+const create = async (req, res, next) => {
 
     req.body.password = passwordToHash(req.body.password)
 
@@ -17,15 +19,15 @@ const create = async (req, res) => {
         const createdUser = await UserService.create(req.body)
         res.status(httpStatus.CREATED).send(createdUser)
     } catch (err) {
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err.message)
+        next(new ApiError(err?.message))
     }
 }
 
-const login = async (req, res) => {
+const login = async (req, res, next) => {
     try {
         req.body.password = passwordToHash(req.body.password)
         let result = await UserService.loginUser(req.body)
-        if (!result) return res.status(httpStatus.NOT_FOUND).send({ message: 'User not found' })
+        if (!result) return next(new ApiError('User not found', 404))
         result = {
             ...result.toObject(),
             access_token: generateAccessToken({ email: result.email, _id: result._id }),
@@ -33,35 +35,34 @@ const login = async (req, res) => {
         }
         res.status((httpStatus.OK)).send(result)
     } catch (err) {
-        console.log(err)
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err)
+        next(new ApiError(err?.message))
     }
 }
 
-const index = async (req, res) => {
+const index = async (req, res, next) => {
     try {
         const users = await UserService.list()
         res.status(httpStatus.OK).send(users)
     } catch (err) {
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err.message)
+        next(new ApiError(err?.message))
     }
 }
 
-const projectList = async (req, res) => {
+const projectList = async (req, res, next) => {
     try {
         const usersProjects = await ProjectService.list({ user_id: req.user })
         res.status(httpStatus.OK).send(usersProjects)
     } catch (err) {
-        res.status(httpStatus.BAD_REQUEST).send(err)
+        next(new ApiError(err?.message))
     }
 }
 
-const resetPassword = async (req, res) => {
+const resetPassword = async (req, res, next) => {
 
     const newPassword = uuid.v4().split('-')[0] || `usr-${new Date().getTime()}`
     try {
         const updatedUser = await UserService.update({ email: req.body.email }, { password: passwordToHash(newPassword) })
-        if (!updatedUser) return res.status(httpStatus.NOT_FOUND).send({ error: "User not found" })
+        if (!updatedUser) return next(new ApiError('User not found', 404))
         eventEmitter.emit('send_email', {
             to: updatedUser.email,
             subject: 'Şifre Sıfırlama',
@@ -71,55 +72,60 @@ const resetPassword = async (req, res) => {
             'message': "Şifre sıfırlama işlemi için sisteme kayıtlı email adresine mail gönderilmiştir"
         })
     } catch (err) {
-        res.status(httpStatus.BAD_REQUEST).send(err)
+        next(new ApiError(err?.message))
     }
 }
 
-const update = async (req, res) => {
+const update = async (req, res, next) => {
     try {
         const updatedUser = await UserService.update({ _id: req.user._id }, req.body)
+        if (!updatedUser)
+            return next(new ApiError('User not found', 404))
         res.status(200).send(updatedUser)
     } catch (err) {
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ err: 'An error occured while updating' })
+        next(new ApiError(err?.message))
     }
 }
 
-const deleteUser = async (req, res) => {
+const deleteUser = async (req, res, next) => {
     try {
         const deletedUser = await UserService.delete({ _id: req.params.id })
         if (!deletedUser) {
-            return res.status(httpStatus.NOT_FOUND).send({ message: "User not found" })
+            return next(new ApiError('User not found', 404))
         }
         res.status(httpStatus.OK).send({ err: 'User deleted' })
     } catch (err) {
-        res.status(httpStatus.BAD_REQUEST).send(err)
+        next(new ApiError(err?.message))
     }
 }
 
-const changePassword = async (req, res) => {
+const changePassword = async (req, res, next) => {
     req.body.password = passwordToHash(req.body.password)
     try {
         const updatedUser = await UserService.update({ _id: req.user._id }, req.body)
+        if (!updatedUser)
+            return next(new ApiError('User not found', 404))
         res.status(200).send(updatedUser)
     } catch (err) {
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ err: 'An error occured while updating' })
+        next(new ApiError(err?.message))
     }
 }
 
-const updateProfileImage = async (req, res) => {
+const updateProfileImage = async (req, res, next) => {
     if (!req?.files?.profile_image) {
-        return res.status(httpStatus.BAD_REQUEST).send({ message: 'Please upload an image' })
+        return next(new ApiError('Please upload an image', 400))
     }
     const extension = path.extname(req.files.profile_image.name)
     const fileName = `${req?.user?._id}${extension}`
     const folderPath = path.join(__dirname, '../uploads/users', fileName)
     req.files.profile_image.mv(folderPath, async function (err) {
-        if (err) return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err)
+        if (err) return next(new ApiError(err?.message))
         try {
             const updatedUser = await UserService.update({ _id: req.user._id }, { profile_image: fileName })
+            if (!updatedUser) return next(new ApiError('User not found', 404))
             res.status(httpStatus.OK).send(updatedUser)
         } catch (err) {
-            res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ message: 'An error occured' })
+            next(new ApiError(err?.message))
         }
     })
 }
